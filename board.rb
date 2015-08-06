bc16daa6144eedd83b9e3fac56b5f603f0297ba4
App Academy Student <appacademy@Apps-Mac-mini-104.local>
2015-08-06 13:27:07 -0400

    Successfully builds board with initial :man positions

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative 'piece'
+
 class Board
   WHITE_SPOT = [
     [0,1], [0,3], [0,5], [0,7],
@@ -16,93 +18,47 @@ class Board
   SLIDE_MOVE = [1,1] # [1,-1], [-1,1],[-1,-1]
   JUMP_MOVE = [2,2] # [2,-2], [-2,2],[-2,-2]
 
-  def initialize(fill_board = true) #also let pieces populate board themselves
-    make_grid(fill_board)
+  attr_reader :grid
+
+  def initialize #also let pieces populate board themselves
+    @grid = Array.new(8) {Array.new(8)}
+    [:white, :red].each do |color|
+      fill_grid(color)
+    end
   end
 
-  def fill_rows(color)
+  def fill_grid(color)
     position_list = (color == :white) ? WHITE_SPOT : RED_SPOTS
 
     position_list.each do |position|
-      piece_class.new(color,self, position)
+      Piece.new(color,self, position)
+      puts "#{position}"
     end
   end
 
-  def move_piece(turn_color, from_pos, *to_pos)
-    # perform checks
-
-
-    move_piece!(from_pos, to_pos.last)
-    jumped_pieces = find_jumped_pieces
-    remove_pieces!(*jumped_pieces)
-  end
-
-  def find_jumped_pieces
-    #
-    jumped_pieces
-  end
-
-  def remove_pieces!(*jumped_pieces)
-    jumped_pieces.each{ |jumped_piece_pos| self[jumped_piece_pos] = nil}
-  end
-
-
-  def move_piece!(from_pos, *to_pos)
-
-    piece = self[from_pos]
-
-    self[to_pos] = piece
-    self[from_pos] = nil
-    piece.pos = to_pos
-    piece_status()
-
-  end
-
-  def move_type(from_pos, *to_pos)
-    from_row, from_col = from_pos
-    to_row, to_col = to_pos.first
-
-    row_change = (to_row - from_row).abs
-    col_change = (to_col - from_col).abs
-
-    change = [row_change, col_change]
-
-    change == [1,1]  ? :SLIDE : :JUMP
-  end
-
-  def perform_slide(color, from_pos, move_type)
-    #if piece is white and is a man row is 1 direction
-    #if piece is red and is a man, row direction is -1 direction
-    row_dir = (color == :white) ? 1 : -1
-    move_change = (move_type == :JUMP) ? JUMP_MOVE : SLIDE_MOVE
-
-    possible_moves = [] #there are only two...if any
+  def [](from_pos)
     row, col = from_pos
-
-    ### THIS DOES NOT YET CHECK IF MOVE IS ON THE BOARD ###
-    # if man
-    possible_moves[0] = [row + row_dir * move_change[0], col + move_change[1]] # right
-    possible_moves[1] = [row + row_dir * move_change[0], col - move_change[1]] # left
-
-    if  self[from_pos].status == :king     # if king
-      possible_moves[2] = [row - row_dir * move_change[0], col + move_change[1]] # right
-      possible_moves[3] = [row - row_dir * move_change[0], col - move_change[1]] # left
-    end
-
-    possible_moves
+    @grid[row][col]
   end
 
+  def []=(pos, piece)
+    row, col = pos
+    @grid[row][col] = piece
+  end
 
   def add_piece(piece, pos)
     self[pos] = piece
   end
 
-  def make_grid(fill_board)
-    @rows  Array.new(8) {Array.new(8)}
-    return unless fill_board
-    [:white, :red].each do |color|
-      fill_rows(color)
-    end
+  def move_piece(turn_color, from_pos, *to_pos)
+    # perform checks
+    piece = self[from_pos]
+    piece.perform_slide(*to_pos)
+    piece.perform_jump(*to_pos)
+
+    # move_piece!(from_pos, to_pos.last)
+    # jumped_pieces = find_jumped_pieces
+    # remove_pieces!(*jumped_pieces)
   end
 
 end
--- piece.rb
+++ piece.rb
@@ -12,14 +12,84 @@ class Piece
     board.add_piece(self, pos)
   end
 
-  def perform_slide
-    #
-
-  end
-
-  def perform_jump
-  end
-
-
+  # def perform_slide
+  #   #
+  #
+  # end
+  #
+  # def perform_jump
+  # end
+  #
+  #
+  # def remove_pieces!(*jumped_pieces)
+  #   jumped_pieces.each{ |jumped_piece_pos| self[jumped_piece_pos] = nil}
+  # end
+  #
+  # def move_piece!(from_pos, *to_pos)
+  #
+  #   piece = self[from_pos]
+  #
+  #   self[to_pos] = piece
+  #   self[from_pos] = nil
+  #   piece.pos = to_pos
+  #   piece_status()
+  # end
+  #
+  #
+  # def perform_slide(*to_pos)
+  #
+  #
+  # end
+  #
+  # def perform_jump(to_pos)
+  # end
+  #
+  # def possible_moves(color, pos)
+  #   #if piece is white and is a man row is 1 direction
+  #   #if piece is red and is a man, row direction is -1 direction
+  #   row_dir = (color == :white) ? 1 : -1
+  #   move_change = (move_type == :JUMP) ? JUMP_MOVE : SLIDE_MOVE
+  #
+  #   possible_moves = [] #there are only two...if any
+  #   row, col = from_pos
+  #
+  #   ### THIS DOES NOT YET CHECK IF MOVE IS ON THE BOARD ###
+  #   # if man
+  #   possible_moves[0] = [row + row_dir * move_change[0], col + move_change[1]] # right
+  #   possible_moves[1] = [row + row_dir * move_change[0], col - move_change[1]] # left
+  #
+  #   if  self[from_pos].status == :king     # if king
+  #     possible_moves[2] = [row - row_dir * move_change[0], col + move_change[1]] # right
+  #     possible_moves[3] = [row - row_dir * move_change[0], col - move_change[1]] # left
+  #   end
+  #
+  #   on_board_moves(possible_moves)
+  # end
+  #
+  # def on_board_moves(possible_moves)
+  #
+  #   valid_moves = possible_moves.select do |move|
+  #
+  #   end
+  #
+  #   valid_moves
+  # end
+  #
+  # def move_type(from_pos, *to_pos)
+  #   from_row, from_col = from_pos
+  #   to_row, to_col = to_pos.first
+  #
+  #   row_change = (to_row - from_row).abs
+  #   col_change = (to_col - from_col).abs
+  #
+  #   change = [row_change, col_change]
+  #
+  #   change == [1,1]  ? :SLIDE : :JUMP
+  # end
+  #
+  #
+  # def on_board?(possible_moves)
+  #
+  # end
 
 end
