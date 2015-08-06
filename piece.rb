18af92c0a9b5786ea62c6c44ac84e8ee218c49d1
App Academy Student <appacademy@Apps-Mac-mini-104.local>
2015-08-06 10:40:09 -0400

    Status added to piece initialize to let us know if piece is a king or a man

--- piece.rb
+++ piece.rb
@@ -1,12 +1,13 @@
 class Piece
   attr_reader :board, :color
-  attr_accessor :pos
+  attr_accessor :pos, :status
 
-  def initialize(color, board, pos)
-    @color, @board, @pos = color, board, pos
+  def initialize(color, board, pos, status = :man)
+    @color, @board, @pos, @status = color, board, pos, status
+    # status can be man or king
     board.add_piece(self, pos)
   end
 
-  
+
 
 end
